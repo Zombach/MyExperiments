f100f48cb502963069e4e1614053de3ec10c107d
Zombach <Kozlov.s.v.1992@gmail.com>
2023-04-17 14:34:39 +0300

    Add class Random

---
+++ MyExperiments/Random.cpp
@@ -0,0 +1,36 @@
+#include "Random.h"
+
+using namespace std;
+
+Random::Random(const unsigned long long min, const unsigned long long max)
+	:min_(min), max_(max)
+{
+	random_device device;
+	const mt19937_64 engine(device());
+	engine_ = engine;
+	const uniform_int_distribution<mt19937_64::result_type> distribution(min_, max_);
+	distribution_ = distribution;
+}
+
+auto Random::get_random(void) -> unsigned long long
+{
+	return distribution_(engine_);
+}
+
+auto Random::set_min(const int min) -> void
+{
+	set_min_max(min, max_);
+}
+
+auto Random::set_max(const int max) -> void
+{
+	set_min_max(min_, max);
+}
+
+auto Random::set_min_max(const int min, const int max) -> void
+{
+	if (min_ != min) { min_ = min; }
+	if (max_ != max) { max_ = max; }
+	const uniform_int_distribution<std::mt19937_64::result_type> distribution(min_, max_);
+	distribution_ = distribution;
+}
---
+++ MyExperiments/Random.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <iostream>
+#include <random>
+
+class Random
+{
+public:
+	explicit Random(const unsigned long long min = 0, const unsigned long long max = 100);
+
+	auto get_random(void) -> unsigned long long;
+	auto set_min(const int min) -> void;
+	auto set_max(const int max) -> void;
+	auto set_min_max(const int min, const int max) -> void;
+
+private:
+	unsigned long long min_;
+	unsigned long long max_;
+	std::mt19937_64 engine_;
+	std::uniform_int_distribution<std::mt19937_64::result_type> distribution_;
+};
